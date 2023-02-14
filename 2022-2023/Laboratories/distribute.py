1d56735c47476ab133b50527570147356a3c5204
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2023-02-14 00:26:12 +0000

    Build and distribution for 2022-2023 lab 2.

--- 2022-2023/Laboratories/distribute.py
+++ 2022-2023/Laboratories/distribute.py
@@ -6,10 +6,13 @@ for rpi in rpinums:
     rpistr = "rpi4-{0:03d}.advopsys.cl.cam.ac.uk".format(rpi)
     print(rpistr + ":")
 
-    cmdstr = ["ssh", "root@" + rpistr, "rm -Rf /advopsys/labs"]
+    cmdstr = ["ssh", "root@" + rpistr, "rm -Rf /advopsys"]
     output = subprocess.run(cmdstr)
-    cmdstr = ["ssh", "root@" + rpistr, "mkdir -p /advopsys/labs"]
+    cmdstr = ["ssh", "root@" + rpistr, "rm -Rf /advopsys-packages/labs"]
+    output = subprocess.run(cmdstr)
+    cmdstr = ["ssh", "root@" + rpistr, "mkdir -p /advopsys-packages/labs"]
     output = subprocess.run(cmdstr)
     cmdstr = ["scp", "2022-2023-advopsys-lab1.tbz",
-      "2022-2023-advopsys-lab2.tbz", "root@" + rpistr + ":/advopsys/labs/"]
+      "2022-2023-advopsys-lab2.tbz",
+      "root@" + rpistr + ":/advopsys-packages/labs/"]
     output = subprocess.run(cmdstr)
